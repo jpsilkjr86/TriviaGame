1c43db41b9988bdcfe1097b7c2884c86bc38c909
John Silkey <John Silkey>
2017-05-08 01:03:56 -0700

    pseudocoded almost all of the entire game object called trivia, which was a lot simpler than i was expecting.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,98 +1,236 @@
 // ********************************************** PSEUDO-CODE **********************************************
 /* 
-	Title: Trivia Game (Advanced)
 
-	MAIN METHODS:
-		1) Timer (object)
-			a) has a set initial start time property
-			b) has a countdown method using setInterval, starts on question screen
-			c) the interval gets cleared when the user makes a guess or when the timer reaches 0
-			d) if the timer reaches 0, unanswered++.
-			e) restarts for each new question
-		2) Questions (array of objects, can display in random order)
-			a) each question has at least four possible answers
-				*only one answer is true, the others are false
-			b) user guesses by clicking on an answer
-			c) has a method where it checks the clicked answer and returns whether it is right or wrong
-			d) if wrong, losses++. if right, wins++.
-			e) status message which tells the user if they are right or wrong, along with the correct answer
-				*can include a gif or other animation
-			f) has a method for printing messages on a div, replacing old text if needed
-			g) has a method for clearning messages on a div so that question screen and status screen
-				appear to be separate pages
-		3) Game Over page (function)
-			a) displays number of correct / incorrect / unsanwered repsonses
-			b) prints button which allows the user to play again
-	
-*/
+TITLE: Trivia Game (Advanced)
 
-// ********************************************* GLOBAL OBJECTS *********************************************
 
 // FUNCTION CONSTRUCTOR FOR QUESTION OBJECTS
 // function questionObj (arg,arg,arg...) {
 // 		this.arg....
 // }
 
-// QUESTION GLOBAL OBJECTS (push onto questions[] array)
+// QUESTION GLOBAL OBJECTS (push onto questionsAry[] array)
 
-// Trivia game housed in an object
+
+$(document).ready(function(){
+
+// Whole trivia game housed in an object called trivia
 var trivia = {
-	initialTime: 30, // initial time set to 30 seconds
+	timeRemaining: 30, // initial time set to 30 seconds
+	remainingQuestions: [], // empty array of remaining questions, which has deep copies of question objects
 	correctAnswers: 0,
 	incorrectAnswers: 0,
 	unansweredQuestions: 0,
 	countDown: function() {
 		// set interval
 
-		// conditions for calling timer.pause()
+		// conditions for calling pauseTimer
 
-			// if interval = 0, call timeOut()
+			// if interval = 0, call  pauseTimer and timeOut()
 
-			// if userwins, pause
 	},
 	resetTimer: function() {
 		// insert reset code here
 	},
 	timeOut: function() {
 		// unansweredQuestions++
+		// calls clearScreen() to remove DOM elements and their data
+		// display messages 
+		// waitforNext()
 	},
 	pauseTimer: function() {
 		// stops the timer where it is at
 	},
-	displayQuestion: function(question) {
-		// takes in question object argument 
+	startGame: function() {
+		// deep copies questionsAry onto remainingQuestions
 
-		// dipslays question and multiple choice options
+		// sets initial values
+		// correctAnswers = 0;
+		// incorrectAnswers = 0;
+		// unansweredQuestions = 0;
+
+		// prints startGame button
+
+		// listens for button click to start the game
+			// calls displayQuestion()
+
+		console.log(trivia);
 	},
-	isAnswerCorrect: function(question, answer) {
-		// takes in question object argument (has correct answers within it) and user's answer
+	displayQuestion: function() {
+		// calls clearScreen(), removes any DOM elements from the board and their data
 
-		// pauses timer right away
+		// calls countDown() to start the timer
 
-		// if correct, return true
+		// pulls a random question from remainingQuestions array, saves as new question
 
-		// if incorrect, return false
-	}
-	displayResult: function(answeredRight) {
-		// answeredRight argument is a bool, method prints corresponding messages
+		// deletes question from remaining questions
+
+		// dipslays question and its multiple choice options
+
+		// assigns option data to the DOM element directly
+
+		// calls getAnswer()
+	},
+	getAnswer: function() {
+		// selector for getting response from user click on the DOM (data stored on DOM already)
+
+			// var result = $(this).data("isItRight");  // data key references a bool
+
+			// calls pauseTimer()
+
+			// displayResult(result);
+	},
+	displayResult: function(result) {
+		// stores DOM element animation / gif if there is any
+
+		// calls clearScreen(), removes DOM elements from the board and their data
+
+		// prints message "Good job!" or "Answer is incorrect" depending on "result"
+
+		// plays gif or animation
+
+		// call waitForNext()
 	},
 	displayGameOver: function() {
+		// calls clearScreen()
+
 		// displays number of correct / incorrect / unsanswered responses
+
+		// prints continue button "Click Here to Continue"
+
+			// listens for button click to call startGame()
 	},
 	clearScreen: function() {
 		// clears the screen so that the displays can appear to be a new "screen" when they are printed
+	},
+	waitForNext: function() {
+		// setInterval timer for 5 seconds before moving to the next screen
+
+		// if remainingQuestions.length === 0
+			// call displayGameOver()
+		// else
+			// call displayQuestion()
 	}
 };
 
-// ******************************************** GLOBAL VARIABLES ********************************************
+trivia.startGame(); // calls startGame() to initialize!
 
-
-// *************************************** GLOBAL METHOD DECLARATIONS ***************************************
+}); // end of Document Ready
 	
+*/  //END OF PSEUDOCODE
 
-// ******************************************* MAIN APP FUNCTIONS *******************************************
+// ******************************************** GLOBAL OBJECTS ********************************************
+
+// FUNCTION CONSTRUCTOR FOR QUESTION OBJECTS
+// function questionObj (arg,arg,arg...) {
+// 		this.arg....
+// }
+
+// QUESTION GLOBAL OBJECTS (push onto questionsAry[] array)
+
 
 $(document).ready(function(){
 
+// Whole trivia game housed in an object called trivia
+var trivia = {
+	timeRemaining: 30, // initial time set to 30 seconds
+	remainingQuestions: [], // empty array of remaining questions, which has deep copies of question objects
+	correctAnswers: 0,
+	incorrectAnswers: 0,
+	unansweredQuestions: 0,
+	countDown: function() {
+		// set interval
 
-}); // end Document Ready
+		// conditions for calling pauseTimer
+
+			// if interval = 0, call  pauseTimer and timeOut()
+
+	},
+	resetTimer: function() {
+		// insert reset code here
+	},
+	timeOut: function() {
+		// unansweredQuestions++
+		// calls clearScreen() to remove DOM elements and their data
+		// display messages 
+		// waitforNext()
+	},
+	pauseTimer: function() {
+		// stops the timer where it is at
+	},
+	startGame: function() {
+		// deep copies questionsAry onto remainingQuestions
+
+		// sets initial values
+		// correctAnswers = 0;
+		// incorrectAnswers = 0;
+		// unansweredQuestions = 0;
+
+		// prints startGame button
+
+		// listens for button click to start the game
+			// calls displayQuestion()
+
+		console.log(trivia);
+	},
+	displayQuestion: function() {
+		// calls clearScreen(), removes any DOM elements from the board and their data
+
+		// calls countDown() to start the timer
+
+		// pulls a random question from remainingQuestions array, saves as new question
+
+		// deletes question from remaining questions
+
+		// dipslays question and its multiple choice options
+
+		// assigns option data to the DOM element directly
+
+		// calls getAnswer()
+	},
+	getAnswer: function() {
+		// selector for getting response from user click on the DOM (data stored on DOM already)
+
+			// var result = $(this).data("isItRight");  // data key references a bool
+
+			// calls pauseTimer()
+
+			// displayResult(result);
+	},
+	displayResult: function(result) {
+		// stores DOM element animation / gif if there is any
+
+		// calls clearScreen(), removes DOM elements from the board and their data
+
+		// prints message "Good job!" or "Answer is incorrect" depending on "result"
+
+		// plays gif or animation
+
+		// call waitForNext()
+	},
+	displayGameOver: function() {
+		// calls clearScreen()
+
+		// displays number of correct / incorrect / unsanswered responses
+
+		// prints continue button "Click Here to Continue"
+
+			// listens for button click to call startGame()
+	},
+	clearScreen: function() {
+		// clears the screen so that the displays can appear to be a new "screen" when they are printed
+	},
+	waitForNext: function() {
+		// setInterval timer for 5 seconds before moving to the next screen
+
+		// if remainingQuestions.length === 0
+			// call displayGameOver()
+		// else
+			// call displayQuestion()
+	}
+};
+
+trivia.startGame(); // calls startGame() to initialize!
+
+}); // end of Document Ready
+
